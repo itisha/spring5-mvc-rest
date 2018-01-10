056cfe14a5e0313010fe15ed117c8d2155e30515
tkuprevich <tkuprevich@exadel.com>
2018-01-10 16:46:43 +0300

    Vendor

---
+++ src/test/java/guru/springfamework/api/v1/mapper/VendorMapperTest.java
@@ -0,0 +1,39 @@
+package guru.springfamework.api.v1.mapper;
+
+import guru.springfamework.api.v1.model.VendorDTO;
+import guru.springfamework.domain.Vendor;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class VendorMapperTest {
+
+    public static final String NAME = "someName";
+
+    VendorMapper vendorMapper = VendorMapper.INSTANCE;
+
+    @Test
+    public void vendorToVendorDTO() {
+        //given
+        Vendor vendor = new Vendor();
+        vendor.setName(NAME);
+
+        //when
+        VendorDTO vendorDTO = vendorMapper.vendorToVendorDTO(vendor);
+
+        //then
+        Assert.assertEquals(vendor.getName(), vendorDTO.getName());
+    }
+
+    @Test
+    public void vendorDTOtoVendor() {
+        //given
+        VendorDTO vendorDTO = new VendorDTO();
+        vendorDTO.setName(NAME);
+
+        //when
+        Vendor vendor = vendorMapper.vendorDTOtoVendor(vendorDTO);
+
+        //then
+        Assert.assertEquals(vendorDTO.getName(), vendor.getName());
+    }
+}
